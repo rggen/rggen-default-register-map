c81e143989413920bec53e593ac4628d9a55f29b
taichi <taichi730@gmail.com>
2019-08-25 23:20:39 +0900

    import from main repository (refs rggen/rggen#2)

---
+++ lib/rggen/default_register_map.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative 'default_register_map/version'
+
+module RgGen
+  module DefaultRegisterMap
+    FEATURES = [
+      'default_register_map/bit_field/name',
+      'default_register_map/register/name',
+      'default_register_map/register_block/name'
+    ].freeze
+
+    def self.load_features
+      FEATURES.each { |file| require_relative file }
+    end
+
+    def self.setup(_builder)
+      load_features
+    end
+  end
+
+  setup :'default-register_map', DefaultRegisterMap
+end
