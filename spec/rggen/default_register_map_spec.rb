a9cf3cfa4dfdf1314dac863d48dd433b338179bb
Taichi Ishitani <taichi730@gmail.com>
2020-07-21 00:14:52 +0900

    added 'comment properties to setup file

--- spec/rggen/default_register_map_spec.rb
+++ spec/rggen/default_register_map_spec.rb
@@ -16,9 +16,9 @@ RSpec.describe RgGen::DefaultRegisterMap do
     it 'フィーチャーの読み出しと有効化を行う' do
       expect(RgGen::DefaultRegisterMap).to receive(:load_features).and_call_original
       expect(builder).to receive(:enable).with(:global, [:bus_width, :address_width]).and_call_original
-      expect(builder).to receive(:enable).with(:register_block, [:name, :byte_size]).and_call_original
-      expect(builder).to receive(:enable).with(:register_file, [:name, :offset_address, :size]).and_call_original
-      expect(builder).to receive(:enable).with(:register, [:name, :offset_address, :size, :type]).and_call_original
+      expect(builder).to receive(:enable).with(:register_block, [:name, :byte_size, :comment]).and_call_original
+      expect(builder).to receive(:enable).with(:register_file, [:name, :offset_address, :size, :comment]).and_call_original
+      expect(builder).to receive(:enable).with(:register, [:name, :offset_address, :size, :type, :comment]).and_call_original
       expect(builder).to receive(:enable).with(:register, :type, [:external, :indirect]).and_call_original
       expect(builder).to receive(:enable).with(:bit_field, [:name, :bit_assignment, :type, :initial_value, :reference, :comment]).and_call_original
       expect(builder).to receive(:enable).with(:bit_field, :type, [:rc, :reserved, :ro, :rof, :rs, :rw, :rwc, :rwe, :rwl, :rws, :w0c, :w0crs, :w0s, :w0src, :w0trg, :w1, :w1c, :w1crs, :w1s, :w1src, :w1trg, :wo, :wo1]).and_call_original
