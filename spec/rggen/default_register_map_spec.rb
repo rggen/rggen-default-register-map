e9a9c55c95ca7dae4208b2463f2c2b510cba2f3b
Taichi Ishitani <taichi730@gmail.com>
2020-08-21 23:06:19 +0900

    added new bit field types to setup file

--- spec/rggen/default_register_map_spec.rb
+++ spec/rggen/default_register_map_spec.rb
@@ -21,7 +21,11 @@ RSpec.describe RgGen::DefaultRegisterMap do
       expect(builder).to receive(:enable).with(:register, [:name, :offset_address, :size, :type, :comment]).and_call_original
       expect(builder).to receive(:enable).with(:register, :type, [:external, :indirect]).and_call_original
       expect(builder).to receive(:enable).with(:bit_field, [:name, :bit_assignment, :type, :initial_value, :reference, :comment]).and_call_original
-      expect(builder).to receive(:enable).with(:bit_field, :type, [:rc, :reserved, :ro, :rof, :rs, :rw, :rwc, :rwe, :rwl, :rws, :w0c, :w0crs, :w0s, :w0src, :w0t, :w0trg, :w1, :w1c, :w1crs, :w1s, :w1src, :w1t, :w1trg, :wo, :wo1]).and_call_original
+      expect(builder).to receive(:enable).with(:bit_field, :type, [
+        :rc, :reserved, :ro, :rof, :rs, :rw, :rwc, :rwe, :rwl, :rws,
+        :w0c, :w0crs, :w0s, :w0src, :w0t, :w0trg, :w1, :w1c, :w1crs, :w1s, :w1src, :w1t, :w1trg,
+        :wo, :wo1, :woc, :wos, :wc, :wcrs, :wrc, :wrs, :ws, :wsrc]
+      ).and_call_original
       builder.load_plugins(['rggen/default_register_map/setup'], true)
     end
   end
