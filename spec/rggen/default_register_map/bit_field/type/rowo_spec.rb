444d7ba647492d4675433094df3d489b1eb710cb
Taichi Ishitani <taichi730@gmail.com>
2022-01-20 22:03:45 +0900

    add reference bit field support to rowo bit field type
    (refs: rggen/rggen#85)

--- spec/rggen/default_register_map/bit_field/type/rowo_spec.rb
+++ spec/rggen/default_register_map/bit_field/type/rowo_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'bit_field/type/rowo' do
   before(:all) do
     RgGen.enable(:register, [:name, :size, :type])
     RgGen.enable(:bit_field, [:name, :bit_assignment, :initial_value, :reference, :type])
-    RgGen.enable(:bit_field, :type, [:rowo])
+    RgGen.enable(:bit_field, :type, [:rowo, :rw])
   end
 
   def create_bit_fields(&block)
@@ -55,18 +55,37 @@ RSpec.describe 'bit_field/type/rowo' do
     }.to raise_register_map_error
   end
 
-  specify '参照ビットフィールドの指定は不要' do
-    expect {
-      create_bit_fields do
-        register do
-          name :foo
-          bit_field { name :foo; bit_assignment lsb: 1; type :rowo; initial_value 0 }
+  context '参照ビットフィールドの指定がある場合' do
+    specify '同一幅のビットフィールドの指定が必要' do
+      expect {
+        create_bit_fields do
+          register do
+            name :foo
+            bit_field { name :foo_0; bit_assignment lsb: 0, width: 1; type :rowo; reference 'bar.bar_0'; initial_value 0 }
+            bit_field { name :foo_1; bit_assignment lsb: 1, width: 2; type :rowo; reference 'bar.bar_1'; initial_value 0 }
+          end
+          register do
+            name :bar
+            bit_field { name :bar_0; bit_assignment lsb: 0, width: 1; type :rw; initial_value 0 }
+            bit_field { name :bar_1; bit_assignment lsb: 1, width: 2; type :rw; initial_value 0 }
+          end
         end
-        register do
-          name :bar
-          bit_field { name :bar; bit_assignment lsb: 1; type :rowo; initial_value 0; reference 'foo.foo' }
+      }.not_to raise_error
+
+      expect {
+        create_bit_fields do
+          register do
+            name :foo
+            bit_field { name :foo_0; bit_assignment lsb: 0, width: 1; type :rowo; reference 'bar.bar_0'; initial_value 0 }
+            bit_field { name :foo_1; bit_assignment lsb: 1, width: 2; type :rowo; reference 'bar.bar_1'; initial_value 0 }
+          end
+          register do
+            name :bar
+            bit_field { name :bar_0; bit_assignment lsb: 0, width: 2; type :rw; initial_value 0 }
+            bit_field { name :bar_1; bit_assignment lsb: 2, width: 1; type :rw; initial_value 0 }
+          end
         end
-      end
-    }.not_to raise_error
+      }.to raise_register_map_error
+    end
   end
 end
