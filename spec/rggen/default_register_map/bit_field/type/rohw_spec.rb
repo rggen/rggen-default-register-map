0e50993e31fdf0a8a857cf350d5b23574f48017b
Taichi Ishitani <taichi730@gmail.com>
2024-01-20 23:47:07 +0900

    rename bit field types
    * rol -> rohw
    * rws -> rwhw

--- spec/rggen/default_register_map/bit_field/type/rohw_spec.rb
+++ spec/rggen/default_register_map/bit_field/type/rohw_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe 'bit_field/type/rol' do
+RSpec.describe 'bit_field/type/rohw' do
   include_context 'clean-up builder'
   include_context 'register map common'
 
@@ -8,29 +8,29 @@ RSpec.describe 'bit_field/type/rol' do
     RgGen.enable(:global, [:bus_width, :enable_wide_register])
     RgGen.enable(:register, [:name, :size, :type])
     RgGen.enable(:bit_field, [:name, :bit_assignment, :initial_value, :reference, :type])
-    RgGen.enable(:bit_field, :type, [:rw, :rol])
+    RgGen.enable(:bit_field, :type, [:rw, :rohw])
   end
 
   def create_bit_fields(&block)
     create_register_map { register_block(&block) }.bit_fields
   end
 
-  specify 'ビットフィールド型は:rol' do
+  specify 'ビットフィールド型は:rohw' do
     bit_fields = create_bit_fields do
       register do
         name 'foo'
-        bit_field { name 'foo'; bit_assignment width: 1; type :rol; initial_value 0 }
+        bit_field { name 'foo'; bit_assignment width: 1; type :rohw; initial_value 0 }
       end
     end
 
-    expect(bit_fields[0]).to have_property(:type, :rol)
+    expect(bit_fields[0]).to have_property(:type, :rohw)
   end
 
   it '揮発性ビットフィールドである' do
     bit_fields = create_bit_fields do
       register do
         name 'foo'
-        bit_field { name 'foo'; bit_assignment width: 1; type :rol; initial_value 0 }
+        bit_field { name 'foo'; bit_assignment width: 1; type :rohw; initial_value 0 }
       end
     end
 
@@ -41,7 +41,7 @@ RSpec.describe 'bit_field/type/rol' do
     bit_fields = create_bit_fields do
       register do
         name 'foo'
-        bit_field { name 'foo'; bit_assignment width: 1; type :rol; initial_value 0 }
+        bit_field { name 'foo'; bit_assignment width: 1; type :rohw; initial_value 0 }
       end
     end
 
@@ -53,7 +53,7 @@ RSpec.describe 'bit_field/type/rol' do
       create_bit_fields do
         register do
           name 'foo'
-          bit_field { name 'foo'; bit_assignment width: 1; type :rol }
+          bit_field { name 'foo'; bit_assignment width: 1; type :rohw }
         end
       end
     }.to raise_register_map_error
@@ -65,7 +65,7 @@ RSpec.describe 'bit_field/type/rol' do
         create_bit_fields do
           register do
             name 'foo'
-            bit_field { name 'foo_0'; bit_assignment lsb: 0, width: 2; type :rol; initial_value 0; reference 'foo.foo_1' }
+            bit_field { name 'foo_0'; bit_assignment lsb: 0, width: 2; type :rohw; initial_value 0; reference 'foo.foo_1' }
             bit_field { name 'foo_1'; bit_assignment lsb: 8, width: 1; type :rw ; initial_value 0 }
           end
         end
@@ -75,7 +75,7 @@ RSpec.describe 'bit_field/type/rol' do
         create_bit_fields do
           register do
             name 'bar'
-            bit_field { name 'bar_0'; bit_assignment lsb: 0, width: 1; type :rol; initial_value 0; reference 'bar.bar_1' }
+            bit_field { name 'bar_0'; bit_assignment lsb: 0, width: 1; type :rohw; initial_value 0; reference 'bar.bar_1' }
             bit_field { name 'bar_1'; bit_assignment lsb: 8, width: 2; type :rw ; initial_value 0 }
           end
         end
--- spec/rggen/default_register_map/bit_field/type/rwhw_spec.rb
+++ spec/rggen/default_register_map/bit_field/type/rwhw_spec.rb
@@ -1,7 +1,7 @@
 
 # frozen_string_literal: true
 
-RSpec.describe 'bit_field/type/rws' do
+RSpec.describe 'bit_field/type/rwhw' do
   include_context 'clean-up builder'
   include_context 'register map common'
 
@@ -9,28 +9,28 @@ RSpec.describe 'bit_field/type/rws' do
     RgGen.enable(:global, [:bus_width, :enable_wide_register])
     RgGen.enable(:register, [:name, :size, :type])
     RgGen.enable(:bit_field, [:name, :bit_assignment, :initial_value, :reference, :type])
-    RgGen.enable(:bit_field, :type, [:rw, :rws])
+    RgGen.enable(:bit_field, :type, [:rw, :rwhw])
   end
 
   def create_bit_fields(&block)
     create_register_map { register_block(&block) }.bit_fields
   end
 
-  specify 'ビットフィール型は:rws' do
+  specify 'ビットフィール型は:rwhw' do
     bit_fields = create_bit_fields do
       register do
         name :foo
-        bit_field { name :foo_0; bit_assignment lsb: 0; type :rws; initial_value 0 }
+        bit_field { name :foo_0; bit_assignment lsb: 0; type :rwhw; initial_value 0 }
       end
     end
-    expect(bit_fields[0]).to have_property(:type, :rws)
+    expect(bit_fields[0]).to have_property(:type, :rwhw)
   end
 
   it '揮発性ビットフィールドである' do
     bit_fields = create_bit_fields do
       register do
         name :foo
-        bit_field { name :foo_0; bit_assignment lsb: 0; type :rws; initial_value 0 }
+        bit_field { name :foo_0; bit_assignment lsb: 0; type :rwhw; initial_value 0 }
       end
     end
     expect(bit_fields[0]).to have_property(:volatile?, true)
@@ -40,7 +40,7 @@ RSpec.describe 'bit_field/type/rws' do
     bit_fields = create_bit_fields do
       register do
         name :foo
-        bit_field { name :foo_0; bit_assignment lsb: 0; type :rws; initial_value 0 }
+        bit_field { name :foo_0; bit_assignment lsb: 0; type :rwhw; initial_value 0 }
       end
     end
     expect(bit_fields[0]).to match_access(:read_write)
@@ -51,7 +51,7 @@ RSpec.describe 'bit_field/type/rws' do
       create_bit_fields do
         register do
           name :foo
-          bit_field { name :foo_0; bit_assignment lsb: 0; type :rws }
+          bit_field { name :foo_0; bit_assignment lsb: 0; type :rwhw }
         end
       end
     }.to raise_register_map_error
@@ -63,13 +63,13 @@ RSpec.describe 'bit_field/type/rws' do
         create_bit_fields do
           register do
             name :foo
-            bit_field { name :foo_0; bit_assignment lsb: 0, width: 2; type :rws; initial_value 0; reference 'foo.foo_1' }
+            bit_field { name :foo_0; bit_assignment lsb: 0, width: 2; type :rwhw; initial_value 0; reference 'foo.foo_1' }
             bit_field { name :foo_1; bit_assignment lsb: 8, width: 1; type :rw; initial_value 0 }
           end
 
           register do
             name :bar
-            bit_field { name :bar_0; bit_assignment lsb: 0, width: 1; type :rws; initial_value 0; reference 'bar.bar_1' }
+            bit_field { name :bar_0; bit_assignment lsb: 0, width: 1; type :rwhw; initial_value 0; reference 'bar.bar_1' }
             bit_field { name :bar_1; bit_assignment lsb: 8, width: 2; type :rw; initial_value 0 }
           end
         end
