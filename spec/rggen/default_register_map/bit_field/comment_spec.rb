60fab7a40024563867b27a75c35677b9e21676a1
Taichi Ishitani <taichi730@gmail.com>
2025-01-22 00:40:48 +0900

    support template comment (#77, rggen/rggen#219)

--- spec/rggen/default_register_map/bit_field/comment_spec.rb
+++ spec/rggen/default_register_map/bit_field/comment_spec.rb
@@ -8,8 +8,22 @@ RSpec.describe 'bit_field/comment' do
     RgGen.enable(:bit_field, :comment)
   end
 
-  def create_bit_field(&block)
-    create_register_map { register_block { register { bit_field(&block) } } }.bit_fields.first
+  let(:bit_field_name) do
+    'foo'
+  end
+
+  def create_bit_field(&)
+    register_map = create_register_map do
+      register_block do
+        register do
+          bit_field(&)
+        end
+      end
+    end
+
+    bit_field = register_map.bit_fields.first
+    allow(bit_field).to receive(:name).and_return(bit_field_name)
+    bit_field
   end
 
   describe '#comment' do
@@ -38,6 +52,24 @@ RSpec.describe 'bit_field/comment' do
       expect(bit_field).to have_property(:comment, '')
     end
 
+    it 'ERBテンプレートとして処理された結果を返す' do
+      bit_field = create_bit_field do
+        comment <<~'COMMENT'
+          this bit field is <%= bit_field.name %>
+          <% 4.times do |i| %>
+          <%= i %>
+          <% end %>
+        COMMENT
+      end
+      expect(bit_field).to have_property(:comment, <<~COMMENT)
+        this bit field is #{bit_field_name}
+        0
+        1
+        2
+        3
+      COMMENT
+    end
+
     context 'コメントが未入力の場合' do
       it '空文字を返す' do
         bit_field = create_bit_field {}
@@ -54,6 +86,22 @@ RSpec.describe 'bit_field/comment' do
       it '入力されたコメントを表示可能オブジェクトとして返す' do
         bit_field = create_bit_field { comment "foo\nbar\nbaz" }
         expect(bit_field.printables[:comment]).to eq "foo\nbar\nbaz"
+
+        bit_field = create_bit_field do
+          comment <<~'COMMENT'
+            this bit field is <%= bit_field.name %>
+            <% 4.times do |i| %>
+            <%= i %>
+            <% end %>
+          COMMENT
+        end
+        expect(bit_field.printables[:comment]).to eq <<~COMMENT
+          this bit field is #{bit_field_name}
+          0
+          1
+          2
+          3
+        COMMENT
       end
     end
 
