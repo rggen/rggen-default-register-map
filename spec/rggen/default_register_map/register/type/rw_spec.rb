204f3a332aae2a0f496e7429b17c279ea2f18fb1
Taichi Ishitani <taichi730@gmail.com>
2025-02-12 23:58:40 +0900

    remove unnecessary feature setting (#80)

--- spec/rggen/default_register_map/register/type/rw_spec.rb
+++ spec/rggen/default_register_map/register/type/rw_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'register/type/rw' do
   include_context 'register map common'
 
   before(:all) do
-    RgGen.enable(:global, [:address_width, :enable_wide_register, :array_port_format])
+    RgGen.enable(:global, [:address_width, :enable_wide_register])
     RgGen.enable(:register_block, [:byte_size, :bus_width])
     RgGen.enable(:register_file, [:name, :offset_address, :size])
     RgGen.enable(:register, [:name, :offset_address, :size, :type])
